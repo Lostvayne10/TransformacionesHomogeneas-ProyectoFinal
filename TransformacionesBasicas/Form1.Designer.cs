70fbaf7072973ac31822d5f9062a8a63dfc26075
lostvayne10 <antonio.infante@alumnos.udg.mx>
2019-05-29 00:20:13 -0500

    Modificando Rotacion
    Verificando el uso de la rotacion, ya implementado
    -Translacion
    -Escalado
    -Reflexion

--- TransformacionesBasicas/Form1.Designer.cs
+++ TransformacionesBasicas/Form1.Designer.cs
@@ -28,12 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.translaciony = new System.Windows.Forms.TextBox();
+            this.translacionx = new System.Windows.Forms.TextBox();
+            this.cbxgrados = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(12, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(744, 490);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBox1.Location = new System.Drawing.Point(764, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(764, 75);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(19, 208);
+            this.vScrollBar1.TabIndex = 2;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(813, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(72, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(787, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nivel de escalado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(775, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 53);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Aplicar Transformacion";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Translacion",
+            "Rotacion",
+            "Escalado",
+            "Reflexion X",
+            "Reflexion Y"});
+            this.comboBox2.Location = new System.Drawing.Point(775, 392);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // translaciony
+            // 
+            this.translaciony.Location = new System.Drawing.Point(775, 364);
+            this.translaciony.Name = "translaciony";
+            this.translaciony.Size = new System.Drawing.Size(121, 22);
+            this.translaciony.TabIndex = 7;
+            this.translaciony.Text = "Translacion en Y";
+            this.translaciony.MouseClick += new System.Windows.Forms.MouseEventHandler(this.translaciony_MouseClick);
+            this.translaciony.TextChanged += new System.EventHandler(this.translaciony_TextChanged);
+            // 
+            // translacionx
+            // 
+            this.translacionx.Location = new System.Drawing.Point(775, 336);
+            this.translacionx.Name = "translacionx";
+            this.translacionx.Size = new System.Drawing.Size(121, 22);
+            this.translacionx.TabIndex = 8;
+            this.translacionx.Text = "Translacion en X";
+            this.translacionx.MouseClick += new System.Windows.Forms.MouseEventHandler(this.translacionx_MouseClick);
+            this.translacionx.TextChanged += new System.EventHandler(this.translacionx_TextChanged);
+            // 
+            // cbxgrados
+            // 
+            this.cbxgrados.FormattingEnabled = true;
+            this.cbxgrados.Items.AddRange(new object[] {
+            "45",
+            "90",
+            "180",
+            "225",
+            "270",
+            "315",
+            "360"});
+            this.cbxgrados.Location = new System.Drawing.Point(775, 306);
+            this.cbxgrados.Name = "cbxgrados";
+            this.cbxgrados.Size = new System.Drawing.Size(121, 24);
+            this.cbxgrados.TabIndex = 9;
+            this.cbxgrados.Text = "Grados";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(1097, 540);
+            this.Controls.Add(this.cbxgrados);
+            this.Controls.Add(this.translacionx);
+            this.Controls.Add(this.translaciony);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox translaciony;
+        private System.Windows.Forms.TextBox translacionx;
+        private System.Windows.Forms.ComboBox cbxgrados;
     }
 }
 
--- TransformacionesBasicas/Form1.cs
+++ TransformacionesBasicas/Form1.cs
@@ -12,9 +12,250 @@ namespace TransformacionesBasicas
 {
     public partial class Form1 : Form
     {
+        Pen pen1 = new Pen(Color.Black, 1);
+        List<Point> lp = new List<Point>();
+        int cont,nump;
+
         public Form1()
         {
             InitializeComponent();
+            cont = 0;
+            nump = 4;
+        }
+
+        public List<Point> Rotacion(List<Point> l, string grados)
+        {
+            List<Point> pr = new List<Point>();
+            double teta = Convert.ToSingle(grados);
+            Point pcentro = GetCentro(l);
+            Point temp;
+            int xe, ye,tx,ty;
+            MessageBox.Show(teta.ToString());
+            for (int i=0;i<l.Count;i++)
+            {
+                xe = (int)((l[i].X - pcentro.X) *(Math.Cos(teta) ) + ((l[i].Y - pcentro.Y) *-Math.Sin(teta)))+ pcentro.X;
+                ye = (int)((l[i].X - pcentro.X) *(Math.Sin(teta) ) + ((l[i].Y - pcentro.Y) * Math.Cos(teta)))+ pcentro.Y;
+                temp = new Point(xe, ye);
+                pr.Add(temp);
+               
+            }
+            /*Point pc = GetCentro(pr);
+            tx = pcentro.X - pc.X;
+            ty = pcentro.Y - pc.Y;
+            pr = Translacion(pr, Convert.ToString(tx), Convert.ToString(ty));*/
+
+            return pr;
+        }
+        public List<Point> Translacion(List<Point> l, string txs, string tys)
+        {
+            List<Point> pr = new List<Point>();
+            Point temp;
+            int  xe, ye,tx,ty;
+            tx = Convert.ToInt32(txs);
+            ty = Convert.ToInt32(tys);
+            xe = ye = 0;
+    
+            for (int i = 0; i < l.Count; i++)
+            {
+                xe = (l[i].X + tx);
+                ye = (l[i].Y + ty);
+                temp = new Point(xe, ye);
+                pr.Add(temp);
+
+            }
+
+            return pr;
+        }
+        public List<Point> Escalado(List<Point> l, float Esc)
+        {
+            List<Point> pr = new List<Point>();
+            Point temp;
+            int xc,yc,xe,ye;
+            xe = ye = 0;
+            xc = GetCentro(l).X;
+            yc = GetCentro(l).Y;
+            for(int i=0;i<l.Count;i++)
+            {
+                xe = (int)Math.Round((l[i].X - xc) * Esc) + xc;
+                ye = (int)Math.Round((l[i].Y - yc) * Esc) + yc;
+                temp = new Point(xe, ye);
+                pr.Add(temp);
+
+            }
+
+            return pr;
+        }
+        public List<Point> ReflexionSobreX(List<Point> l)
+        {
+            List<Point> pr = new List<Point>();
+            Point temp;
+            Point pcentro = GetCentro(l);
+            int  xe, ye;
+            xe = ye = 0;
+            for (int i=0;i<l.Count;i++)
+            {
+                xe = l[i].X;
+                ye = ((pcentro.Y - l[i].Y) * 2)+ l[i].Y;
+                temp = new Point(xe,ye);
+                pr.Add(temp);
+            }
+
+            return pr;
+        }
+        public List<Point> ReflexionSobreY(List<Point> l)
+        {
+            List<Point> pr = new List<Point>();
+            Point temp;
+            Point pcentro = GetCentro(l);
+            int xe, ye;
+            xe = ye = 0;
+            for (int i = 0; i < l.Count; i++)
+            {
+                ye = l[i].Y;
+                xe = ((pcentro.X - l[i].X) * 2) + l[i].X;
+                temp = new Point(xe, ye);
+                pr.Add(temp);
+            }
+
+            return pr;
+        }
+
+        public Point GetCentro(List<Point> l)
+        {
+            Point p ;
+            int xm, ym, xs, ys,xc,yc;
+            xm = ym = 100000;
+            ys = xs = 0 ;
+            for (int i=0;i<l.Count;i++)
+            {
+                if(xm>l[i].X)
+                {
+                    xm = l[i].X;
+                }
+                if (ym > l[i].Y)
+                {
+                    ym = l[i].Y;
+                }
+                if (xs < l[i].X)
+                {
+                    xs = l[i].X;
+                }
+                if (ys < l[i].Y)
+                {
+                    ys = l[i].Y;
+                }
+            }
+            xc = ((xs - xm) / 2) + xm;
+            yc = ((ys - ym) / 2) + ym;
+            p = new Point(xc, yc);
+
+            return p;
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            Point p = new Point(e.X, e.Y);
+            if(cont< nump)
+            {
+                panel1.CreateGraphics().DrawEllipse(pen1, e.X, e.Y, 1, 1);
+                lp.Add(p);
+                cont++;
+            }
+            if(cont== nump)
+            {
+                DibujarFigura(lp);
+                panel1.CreateGraphics().DrawEllipse(pen1, GetCentro(lp).X, GetCentro(lp).Y, 1, 1);
+                DibujarCentro(lp);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            nump = Convert.ToInt32(comboBox1.Text);
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            
+            textBox1.Text = (vScrollBar1.Value / 10).ToString();
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.CreateGraphics().Clear(Color.White);
+            if (comboBox2.SelectedIndex == 0)
+            {
+                DibujarFigura(Translacion(lp, translacionx.Text,translaciony.Text));
+                DibujarCentro(lp);
+            }
+            if (comboBox2.SelectedIndex == 1)
+            {
+                DibujarFigura(Rotacion(lp, cbxgrados.Text));
+                DibujarCentro(Rotacion(lp, cbxgrados.Text));
+            }
+            if (comboBox2.SelectedIndex==2)
+            {
+                DibujarFigura(Escalado(lp, (float)(vScrollBar1.Value / 10)));
+                DibujarCentro(lp);
+            }
+            if(comboBox2.SelectedIndex == 3)
+            {
+                DibujarFigura(ReflexionSobreX(lp));
+                DibujarCentro(lp);
+            }
+            if (comboBox2.SelectedIndex == 4)
+            {
+                DibujarFigura(ReflexionSobreY(lp));
+                DibujarCentro(lp);
+            }
+        }
+
+        private void translacionx_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void translaciony_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void translacionx_MouseClick(object sender, MouseEventArgs e)
+        {
+            translacionx.Text = "";
+        }
+
+        private void translaciony_MouseClick(object sender, MouseEventArgs e)
+        {
+            translaciony.Text = "";
+        }
+
+        public void DibujarFigura(List<Point> l)
+        {
+            Point[] Arr = new Point[l.Count+1];
+            for (int i=0;i<lp.Count;i++)
+            {
+                Arr[i] = l[i];
+            }
+            Arr[l.Count] = l[0];
+            panel1.CreateGraphics().DrawLines(pen1, Arr);
+        }
+
+        public void DibujarCentro(List<Point> l)
+        {
+            Point center = GetCentro(l);
+            panel1.CreateGraphics().DrawLine(pincel(Color.Red), center.X - 15, center.Y, center.X + 15, center.Y);
+            panel1.CreateGraphics().DrawLine(pincel(Color.Red), center.X , center.Y-15, center.X , center.Y+15);
+
+        }
+
+        Pen pincel(Color a)
+        {
+            Pen pincel = new Pen(a, 1);
+            return pincel;
         }
     }
 }
